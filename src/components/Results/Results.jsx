7bbf48481f7a840adc7550f6fc8c266c93bd8bb1
emericsoquet <soquet.emeric@gmail.com>
2024-02-25 23:05:19 +0100

    feat(results): Get streamer infos after typing login

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,16 +1,45 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {Link, useParams} from 'react-router-dom';
+import { getUserByLogin } from '../../services/live.service';
 
 export default function Results() {
 
     let { slug } = useParams();
 
     const [result, setResult] = useState(true);
+    const [streamer, setStreamer] = useState([]);
 
+    let cleanSearch = slug.replace(/ /g, '');
+
+    useEffect( () => {
+        const fetchData = async () => {
+            const data = await getUserByLogin(cleanSearch);
+            setStreamer(data);
+        } 
+        fetchData();
+    }, [slug])
+
+    console.log(streamer);
 
     return (
         <>
-            <h1>bjr</h1>
+            <div className="results">
+                { streamer.map( (streamer, index) => (
+                    <Link className="results__card" key={index}
+                        to={{
+                            pathname: `/live/${streamer.login}`
+                        }}
+                    >
+                        <figure>
+                            <img src={streamer.profile_image_url} alt="" />
+                        </figure>
+                        <figcaption>
+                            <h3>{streamer.display_name}</h3>
+                            <p>{streamer.description}</p>
+                        </figcaption>
+                    </Link>
+                ))}
+            </div>
         </>
     )
 }
