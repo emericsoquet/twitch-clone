702ed32cb9614b1b08a42f156580ce1273bec6d0
emericsoquet <soquet.emeric@gmail.com>
2024-02-26 02:01:11 +0100

    style(results): Manage infos in cards

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -4,14 +4,21 @@ export default function Error() {
     let { slug } = useParams();
     return (
         <div className="results">
-            <h1>Oops</h1>
-            <p>
-                { slug ? 
-                        `Unfortunately, there is no match for ${ slug }. Check you didn't mispell what you were looking for.` 
-                        : `Please type something in the searchbar to get a result.`
-                }
-                </p>
-            <Link to={{ pathname: `/`}}>Homepage</Link>
+            <div className="results__container">
+                <div className="results__wrapper">
+                    <div className="results__error">
+                        <h1>Oops</h1>
+                        <p>
+                            { slug ? 
+                                    `Unfortunately, there is no match for ${ slug }. Check you didn't mispell what you were looking for.` 
+                                    : `Please type something in the searchbar to get a result.`
+                            }
+                            </p>
+                        <Link className="cta" to={{ pathname: `/`}}>Homepage</Link>
+                    </div>
+                    </div>
+                </div>
+                
         </div>
     )
 }
--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import {Link, useParams} from 'react-router-dom';
-import { getUserByLogin } from '../../services/live.service';
+import { getLiveStream, getUserByLogin } from '../../services/live.service';
 import Error from '../Error/Error'
 
 export default function Results() {
@@ -14,38 +14,58 @@ export default function Results() {
 
     useEffect( () => {
         const fetchData = async () => {
-            const data = await getUserByLogin(cleanSearch);
+            const streamerData = await getUserByLogin(cleanSearch);
 
-            if(data.length === 0) {
+            if(streamerData.length === 0) {
                 setResult(false);
             } else {
                 setResult(true);
-                setStreamer(data);
+
+                let streamerLogin = streamerData[0].login;
+                const streamerStatus = await getLiveStream(streamerLogin);
+
+                console.log(streamerStatus)
+
+                if(streamerStatus.length === 0) {
+                    setStreamer(streamerData.map( streamer => ({...streamer, status: false})))
+                } else {
+                    setStreamer(streamerData.map( streamer => ({...streamer, status: true})))
+                }
+                
+
             }
         } 
         fetchData();
     }, [cleanSearch])
+
+    console.log(streamer)
 
     return (
 
         result ? 
             <>
                 <div className="results">
-                    { streamer.map( (streamer, index) => (
-                        <Link className="results__card" key={index}
-                            to={{
-                                pathname: `/live/${streamer.login}`
-                            }}
-                        >
-                            <figure>
-                                <img src={streamer.profile_image_url} alt="" />
-                            </figure>
-                            <figcaption>
-                                <h3>{streamer.display_name}</h3>
-                                <p>{streamer.description}</p>
-                            </figcaption>
-                        </Link>
-                    ))}
+                    <div className="results__container">
+                        <div className="results__wrapper">
+                            { streamer.map( (streamer, index) => (
+                                <Link className="results__card" key={index}
+                                    to={{
+                                        pathname: `/live/${streamer.login}`
+                                    }}
+                                >
+                                    <figure>
+                                        <img src={streamer.profile_image_url} alt="" />
+                                    </figure>
+                                    <figcaption>
+                                        <h3>{streamer.display_name}</h3>
+                                        <p>{streamer.description}</p>
+                                    </figcaption>
+
+                                    { streamer.status ?  <span className="results__status">Live</span> : '' }
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </>
         : 
